690852c69034ec18d8922f90b131cece3048db6c
boruch d <dubywider@gmail.com>
2019-06-04 17:02:46 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,11 +1,9 @@
 class MP3Importer
-  attr_reader :path
-  
+  attr_accessor :path
   def initialize(path)
-    self.path = path 
-  end 
-  
- def files
+    self.path = path
+  end
+  def files
     path = self.path + "/*.mp3"
     processed = Dir[path].collect do |path_and_name|
       path_and_name.split("/").last
@@ -17,6 +15,7 @@ class MP3Importer
       Song.create_from_filename(filename)
     end
   end
-
+end 
+  
 
 end 
